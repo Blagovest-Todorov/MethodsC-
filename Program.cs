d290c93d44c9baee1aa515f050fc3de0720e6b54
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-02-09 08:28:27 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumb = 5;
+            int secNumb = 10;
+
+            int sum = Getsum(firstNumb, secNumb);
+
+            Console.WriteLine(sum);
+
+        }
+
+        private static int Getsum(int first, int sec)
+        {
+            return first + sec;
+        }
+
+        //Methods we reuse the code, make the code better readable, 
+        // better maitainability, devide the big problem into small
+        // problems.
+    }
+}
